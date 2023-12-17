749b14b3e36a5c96cdb9e5a23a161d9ff88071ec
krishnaprasad <krishnaprasad8369@gmail.com>
2023-12-18 02:00:45 +0530

    completed

--- src/Components/Cards/Cards.jsx
+++ src/Components/Cards/Cards.jsx
@@ -13,10 +13,12 @@ const Cards = () => {
   return (
     <>
       <main>
+   
         <div className="main-page relative">
+
           <div className="h-[80px]">
             <div className="flex justify-between ">
-              <img className="logo-img" src={logo} alt="" />
+              <img className="logo-img " src={logo} alt="" />
               <div className="flex justify-center flex-wrap gap-3  ">
               <button className="flex flex-wrap pt-2 mt-2 bg-white justify-center align-middle rounded-md text-[12px] h-10 w-[160px]">
                 <img className="w-[20px]" src={ai} alt="" />
@@ -27,32 +29,36 @@ const Cards = () => {
               </button>
             </div>
             </div>
-
-           
+          
             <div className=" w-[45px] right-[-4px] top-25 align-middle">
-              <span className="absolute right-0  text-cyan-600  ">
+              <span className="bell absolute text-[40px] right-3  text-cyan-600  ">
                 <FaBell />
               </span>
             </div>
           </div>
-          <div className="sub-main bg-white p-6 rounded-lg shadow-md">
-            <h2 className="text-2xl font-bold px-12 mb-4">
+          <div className="sub-main flex justify-between bg-white p-6 rounded-lg shadow-md">
+           
+            <div className="side">
+              <Sidebar/>
+            </div>
+            <div className="sub-side" >
+            <h2 className="title text-2xl font-bold px-12 mb-4">
               Choose a plan that's just right for you!
             </h2>
             <div className="head-btn">
-              <h3 className="bg-cyan-100 w-[140px] h-8 rounded-2xl text-center">
+              <h3 className="month bg-cyan-100 w-[140px] h-8 rounded-2xl text-center">
                 Monthly
               </h3>
 
-              <h3>
+              <h3 className="flex">
                 Annually
                 <span className="text-green-500 text-[10px] ">-10%</span>
               </h3>
             </div>
-            <div className="flex flex-wrap mt-20 align-middle justify-center">
+            <div className=" cards flex flex-wrap mt-20 align-middle justify-center">
               <div className="max-w-sm w-[300px]  rounded overflow-hidden shadow-lg m-4">
-                <div className="px-6 py-4">
-                  <div className="font-bold text-[25px] mb-2">Basic</div>
+                <div className="card px-6 py-4">
+                  <h2 className="font-bold text-[25px] mb-2">Basic</h2>
                   <p className="line-through text-sm text-red-500">$89.99/mo</p>
                   <h2 className="text-[26px]">$9.99/mo</h2>
                   <button className="flex mt-3 justify-center h-9 pt-1 rounded-md bg-orange-300 w-[130px] gap-4">
@@ -95,8 +101,8 @@ const Cards = () => {
               </div>
 
               <div className="max-w-sm w-[300px]  rounded overflow-hidden shadow-lg m-4">
-                <div className="px-6 py-4">
-                  <div className="font-bold text-[25px] mb-2">Standard</div>
+                <div className="card px-6 py-4">
+                  <h2 className="font-bold text-[25px] mb-2">Standard</h2>
                   <p className="line-through text-sm text-red-500">
                     $189.99/mo
                   </p>
@@ -140,8 +146,8 @@ const Cards = () => {
                 </div>
               </div>
               <div className="max-w-sm w-[300px]  rounded overflow-hidden shadow-lg m-4">
-                <div className="px-6 py-4">
-                  <div className="font-bold text-[25px] mb-2">Premium</div>
+                <div className="card px-6 py-4">
+                  <h2 className="font-bold text-[25px] mb-2">Premium</h2>
                   <p className="line-through text-sm text-red-500">
                     $359.99/mo
                   </p>
@@ -188,7 +194,7 @@ const Cards = () => {
             <section>
               <div className="flex flex-wrap">
                 <div className="max-w-lg mt-3 mx-auto bg-white shadow-md rounded-md p-6">
-                  <div className="flex flex-col md:flex-row">
+                  <div className="card flex flex-col md:flex-row">
                     <div className="md:w-1/2 md:pr-4 mb-4 md:mb-0">
                       <h4 className="text-lime-700 bg-lime-400 w-[112px] text-center h-9 pt-1 rounded-2xl">
                         Free Forever
@@ -282,6 +288,7 @@ const Cards = () => {
                 </div>
               </div>
             </section>
+            </div>
           </div>
         </div>
       </main>
--- src/Components/Sidebar/Sidebar.jsx
+++ src/Components/Sidebar/Sidebar.jsx
@@ -1,70 +1,98 @@
-import React from "react";
+import React, { useState } from "react";
 import { MdOutlineArrowForwardIos } from "react-icons/md";
+import { TbGridDots } from "react-icons/tb";
 import { ImSwitch } from "react-icons/im";
 import icon1 from "../../images/icon1.png";
 import icon2 from "../../images/icon2.png";
 import icon3 from "../../images/icon3.png";
 import icon4 from "../../images/icon4.png";
 import icon5 from "../../images/icon5.png";
-import logo from "../../images/logo.png";
 import profile from "../../images/profile.png";
 import "./Sidebar.css";
 
 const Sidebar = () => {
+  
+  const [sidebarOpen, setSidebarOpen] = useState(false);
+
+  const toggleSidebar = () => {
+    setSidebarOpen(!sidebarOpen);
+  };
+
+
   return (
     <nav>
-      <section className="">
-        <img className="logo-img " src={logo} alt="" />
-        <div className="main shadow-2xl  ">
-          <div className=" profile max-w-xs   w-[140px] h-[170px] bg-white rounded-xl overflow-hidden shadow-2xl">
-            <div className="prfl  p-4 ">
-              <div>
-                <img
-                  src={profile}
-                  alt=""
-                  className="rounded-full w-20 h-20 mx-auto mb-4"
-                />
-                <h3 className=" flex flex-wrap items-center justify-evenly font-bold text-[12px]  mb-2">
-                  Ram Mohan
-                  <span className="text-cyan-700">
-                    <MdOutlineArrowForwardIos />
-                  </span>
-                </h3>
-                <h5 className=" text-gray-600 text-[10px]">
-                  rammohan@gmail.com
-                </h5>
-              </div>
-            </div>
-          </div>
-          <div className="buttons mt-2 ">
-            <div className="btn gap-5 bg-cyan-700">
-              <img src={icon1} alt="" />
-              <h3>Dashboard</h3>
-            </div>
-            <div className="btn gap-5">
-              <img src={icon2} alt="" />
-              <h3>Perks</h3>
-            </div>
-            <div className="btn gap-5">
-              <img src={icon3} alt="" />
-              <h3>Addons</h3>
-            </div>
-            <div className="btn gap-5">
-              <img src={icon4} alt="" />
-              <h3>FAQ</h3>
-            </div>
-            <div className="btn gap-5">
-              <img src={icon5} alt="" />
-              <h3>Support</h3>
-            </div>
-          </div>
-          <div className="logbtn px-2">
-            <button className="">Logout </button>
-            <ImSwitch className="icon w-[15px] mt-" />
-          </div>
-        </div>
-      </section>
-    </nav>
+       <button className="block mt-4 md:hidden " onClick={toggleSidebar}>
+        < TbGridDots />
+        </button>
+    <div className="flex h-screen">
+ 
+   
+   <aside className={`bar bg-white w-50 shadow-2xl ${sidebarOpen ? 'block' : 'hidden'} md:block`}>
+   
+     <div className=" p-4 text-blue-950">
+       
+       <div>
+       <div className=" profile max-w-xs   w-[140px] h-[170px] bg-white rounded-xl overflow-hidden shadow-2xl">
+         <div className="prfl  p-4 ">
+           <div>
+             <img
+               src={profile}
+               alt=""
+               className="rounded-full w-20 h-20 mx-auto mb-4"
+             />
+             <h3 className=" flex text-gray-600 flex-wrap items-center justify-evenly font-bold text-[12px]  mb-2">
+               Ram Mohan
+               <span className="text-cyan-700">
+                 <MdOutlineArrowForwardIos />
+               </span>
+             </h3>
+             <h5 className=" text-gray-600 text-[10px]">
+               rammohan@gmail.com
+             </h5>
+           </div>
+
+           
+           
+         </div>
+       </div>
+       </div>
+       <ul className="mt-4">
+        
+         <div className="buttons   mt-2 ">
+         <div className="btn flex rounded-md gap-5 bg-cyan-700">
+           <img className="w-[30px]" src={icon1} alt="" />
+           <h3>Dashboard</h3>
+         </div>
+         <div className="btn flex mt-4 gap-5">
+           <img className="w-[30px]" src={icon2} alt="" />
+           <h3>Perks</h3>
+         </div>
+         <div className="btn  mt-4 flex gap-5">
+           <img className="w-[30px]" src={icon3} alt="" />
+           <h3>Addons</h3>
+         </div>
+         <div className="btn  mt-4 flex gap-5">
+           <img className="w-[30px]" src={icon4} alt="" />
+           <h3>FAQ</h3>
+         </div>
+         <div className="btn mt-4 flex gap-5">
+           <img className="w-[30px]" src={icon5} alt="" />
+           <h3>Support</h3>
+         </div>
+         <div className="flex mt-[150px] gap-3 px-2">
+         <button className="">Logout </button>
+         <ImSwitch className="icon w-[15px] mt-1" />
+       </div>
+       </div>
+       
+       </ul>
+      
+     </div>
+   </aside>
+ </div>
+   
+ 
+ </nav> 
   );
 };
 
